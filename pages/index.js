515ea6b80d586541347a396bc8682b8678ac0620
mikaelcantini <84602540+MikaelCantini@users.noreply.github.com>
2025-10-23 10:33:22 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>O BORGES , O BRENDON E O TESSMANN SAO GAYZÃO</h1>
+    return <h1>🤖🤖🤖 O BORGES , O BRENDON E O TESSMANN SAO GAYZÃO 🤖🤖🤖</h1>
 }
 
 export default Home;
